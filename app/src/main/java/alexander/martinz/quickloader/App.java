6d18727953fbb73c7448c7faa0e702f40ba1cfea
Alexander Martinz <eviscerationls@gmail.com>
2016-06-16 10:00:24 +0200

    Add UniversalDebug, setup Fabric
    Change-Id: I64e3860bafd851469cea6dc986a21a6e3964cd6e
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

---
+++ app/src/main/java/alexander/martinz/quickloader/App.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 - 2016 Alexander Martinz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package alexander.martinz.quickloader;
+
+import android.app.Application;
+
+import at.amartinz.universaldebug.UniversalDebug;
+import at.amartinz.universaldebug.fabric.FabricConfig;
+
+public class App extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+
+        final UniversalDebug universalDebug = new UniversalDebug(this)
+                .withDebug(BuildConfig.DEBUG)
+                .withTimber(false);
+
+        final FabricConfig fabricConfig = new FabricConfig(universalDebug)
+                .withAnswers()
+                .withCrashlytics();
+        universalDebug.withExtension(fabricConfig);
+
+        universalDebug.install();
+    }
+}
